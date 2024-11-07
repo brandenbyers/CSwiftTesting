414f6bb1a05ae84fee5f1891b0e75a281f6e0329
Branden Byers <branden.byers@agilebits.com>
2024-11-06 20:56:12 -0800

    Complete flattening

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Cee"]),
     ],
     targets: [
-        .target(name: "Cee", path: "src"),
+        .target(name: "Cee", path: "src", publicHeadersPath: "includes"),
         .testTarget(
             name: "CeeTests",
             dependencies: ["Cee"],
---
+++ src/includes/Bridging-Header.h
@@ -0,0 +1,1 @@
+#include "../cee.h"
